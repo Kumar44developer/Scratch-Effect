46ff45f694d00fa6eace86cb46cb5510f91725d4
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-05 13:26:31 +0530

    Implemented mouse tracking and position scratcher element based on cursor offset

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener("mousemove",(e)=>{
  const body = document.querySelector("body");
  const scratcher = document.createElement("span");
+  scratcher.style.left = -17 + e.offsetX + "px";
