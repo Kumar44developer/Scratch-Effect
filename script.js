3a28826b1b4c5385cae9bd8a308cc7f5cc1d9e3d
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-05 13:27:09 +0530

    Added mousemove handler and update scratcher element position using cursor offsetX and offsetY

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.addEventListener("mousemove",(e)=>{
  const body = document.querySelector("body");
  const scratcher = document.createElement("span");
   scratcher.style.left = -17 + e.offsetX + "px";
+scratcher.style.top = -17 + e.offsetY + "px";
